ba57823e06b7fe0eb90fd6ddea6e9f55897d120d
Rob Antoine <rjantoine@gmail.com>
2025-08-29 00:04:32 +0000

    The KaTeX HTML seems to be working correctly, but it is not actually dis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import 'katex/dist/katex.min.css';
 
 export const metadata: Metadata = {
   title: "MathType Clipboard",
@@ -18,12 +19,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata&display=swap" rel="stylesheet" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css"
-          integrity="sha384-fHo4NVI1U//3B2BwVnT2a2C1i7B3nC2aXWn4B4DAljYVzo1p+i3kL2MVI00HlS+l"
-          crossOrigin="anonymous"
-        />
       </head>
       <body className="font-body antialiased">
         {children}
